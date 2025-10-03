6134e39636d963e6d7c32975f8f3c88c5f660bf8
xsoder <tadihailukebe@gmail.com>
2025-10-03 16:56:43 -0400

    Addition of flake for my suckless tool

---
+++ flake.nix
@@ -0,0 +1,160 @@
+{
+  description = "Suckless software builds";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        
+        # Helper function to build suckless software
+        buildSuckless = { pname, version, src, extraBuildInputs ? [], meta ? {} }: 
+          pkgs.stdenv.mkDerivation {
+            inherit pname version src;
+            
+            buildInputs = with pkgs; [
+              xorg.libX11
+              xorg.libXft
+              xorg.libXinerama
+              fontconfig
+              freetype
+            ] ++ extraBuildInputs;
+            
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+            
+            prePatch = ''
+              sed -i "s@/usr/local@$out@" config.mk
+            '';
+            
+            makeFlags = [
+              "PREFIX=$(out)"
+              "CC=${pkgs.stdenv.cc.targetPrefix}cc"
+            ];
+            
+            installPhase = ''
+              make PREFIX=$out install
+            '';
+            
+            meta = with pkgs.lib; meta // {
+              homepage = meta.homepage or "https://suckless.org";
+              license = licenses.mit;
+              platforms = platforms.linux;
+            };
+          };
+        
+        # Build dmenu
+        dmenu-custom = buildSuckless {
+          pname = "dmenu-custom";
+          version = "5.4";
+          src = ./dmenu-5.4;
+          meta.description = "Dynamic menu for X (customized with center patch)";
+        };
+        
+        # Build st (simple terminal)
+        st-custom = buildSuckless {
+          pname = "st-custom";
+          version = "0.9.2";
+          src = ./st;
+          meta.description = "Simple terminal implementation for X";
+        };
+        
+        # Build dwm (dynamic window manager)
+        dwm-custom = buildSuckless {
+          pname = "dwm-custom";
+          version = "6.5";
+          src = ./dwm;
+          meta.description = "Dynamic window manager for X";
+        };
+        
+      in
+      {
+        packages = {
+          dmenu = dmenu-custom;
+          st = st-custom;
+          dwm = dwm-custom;
+          default = dmenu-custom;
+          
+          # Meta-package that builds everything
+          all = pkgs.symlinkJoin {
+            name = "suckless-all";
+            paths = [ dmenu-custom st-custom dwm-custom ];
+          };
+        };
+        
+        devShells.default = pkgs.mkShell {
+          name = "suckless-dev";
+          
+          packages = with pkgs; [
+            # Build tools
+            pkg-config
+            gcc
+            gnumake
+            git
+            
+            # X11 libraries
+            xorg.libX11
+            xorg.libXft
+            xorg.libXinerama
+            
+            # Font libraries
+            fontconfig
+            freetype
+            harfbuzz
+            
+            # Debugging and development
+            gdb
+            valgrind
+            
+            # Patch management
+            patchutils
+            diffutils
+          ];
+          
+          shellHook = ''
+            echo "Suckless development environment"
+            echo "================================"
+            echo ""
+            echo "Available projects:"
+            echo "  - dmenu-5.4/"
+            echo "  - st/"
+            echo "  - dwm/"
+            echo ""
+            echo "Build individually:"
+            echo "  nix build .#dmenu"
+            echo "  nix build .#st"
+            echo "  nix build .#dwm"
+            echo ""
+            echo "Build all at once:"
+            echo "  nix build .#all"
+            echo ""
+          '';
+          
+          # Set up environment variables
+          XINERAMA = "1";
+          FREETYPEINC = "${pkgs.freetype.dev}/include/freetype2";
+          X11INC = "${pkgs.xorg.libX11.dev}/include";
+          X11LIB = "${pkgs.xorg.libX11}/lib";
+        };
+        
+        # Alternative dev shell with more minimal setup
+        devShells.minimal = pkgs.mkShell {
+          packages = with pkgs; [
+            pkg-config
+            xorg.libX11
+            xorg.libXft
+            xorg.libXinerama
+            fontconfig
+            freetype
+            gcc
+            gnumake
+          ];
+        };
+      }
+    );
+}
