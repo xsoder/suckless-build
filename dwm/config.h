66307c898865fe49d9a516dd4ba8ff19e4b84d95
xsoder <tadihailukebe@gmail.com>
2025-11-04 09:12:27 -0500

    Add slstatus source directly

--- dwm/config.h
+++ dwm/config.h
@@ -2,11 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 0;        /* gaps between windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka:size=14" };
+static const char *fonts[]          = { "IosevkaNerdfont:size=10" };
 static const char dmenufont[]       = "Iosevka:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
